28e7653bdaeafdc15ec2b62fef890adc6e18ed3d
pablosingh <pablo22_4@hotmail.com>
2024-07-25 20:36:55 -0300

    get y post

--- src/controllers/operations.controllers.js
+++ src/controllers/operations.controllers.js
@@ -3,23 +3,30 @@ import Operation from "../models/Operation.js";
 export const createOperation = async (req, res) => {
     console.log(req.body);
     const { date, ticker, amount, price, total, buy, exchange, comment } = req.body;
-
-    const newOperation = await Operation.create({ 
-        // date,
-        date: new Date(),
-        ticker, 
-        amount, 
-        price, 
-        total, 
-        buy, 
-        exchange, 
-        comment 
-});
-    console.log("nueva op");
-    console.log(newOperation);
-    res.send('creating operation');
+    try {
+        const newOperation = await Operation.create({ 
+            // date,
+            date: new Date(),
+            ticker, 
+            amount, 
+            price, 
+            total, 
+            buy, 
+            exchange, 
+            comment 
+        });
+        console.log("nueva op");
+        res.json(newOperation);
+    } catch (error) {
+        res.json({message: error});
+    }
 };
 
-export const getOperation = (req, res) => {
-    res.send('getting operation');
+export const getOperation = async (req, res) => {
+    try {
+        const arrayOp = await Operation.findAll();
+        res.json(arrayOp);   
+    } catch (error) {
+        res.json({message: error});
+    }
 };
