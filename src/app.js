c178ec44ebd9000000faddb2ed1850256bd9a8cd
pablosingh <pablo22_4@hotmail.com>
2024-07-24 23:24:09 -0300

    testing primeras rutas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import operationsRoutes from './routes/operations.routes.js';
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+
 app.use(operationsRoutes);
 
 export default app;
--- src/controllers/operations.controllers.js
+++ src/controllers/operations.controllers.js
@@ -1,4 +1,22 @@
-export const createOperation = (req, res) => {
+import Operation from "../models/Operation.js";
+
+export const createOperation = async (req, res) => {
+    console.log(req.body);
+    const { date, ticker, amount, price, total, buy, exchange, comment } = req.body;
+
+    const newOperation = await Operation.create({ 
+        // date,
+        date: new Date(),
+        ticker, 
+        amount, 
+        price, 
+        total, 
+        buy, 
+        exchange, 
+        comment 
+});
+    console.log("nueva op");
+    console.log(newOperation);
     res.send('creating operation');
 };
 
