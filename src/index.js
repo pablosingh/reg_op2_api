506e6aa96b5f08f4f7c8281a603f41b5c71c498a
pablosingh <pablo22_4@hotmail.com>
2024-08-15 20:16:12 -0300

    app basica

--- src/index.js
+++ src/index.js
@@ -8,21 +8,32 @@ import Operation from './models/Operation.js';
 import Cripto from './models/Cripto.js';
 import User from './models/User.js';
 
-User.hasMany(Holding, { foreinkey: "UserId" });
-Holding.belongsTo(User, { foreignKey: 'UserId'});
+// User.hasMany(Holding, { foreinkey: "UserId" });
+// Holding.belongsTo(User, { foreignKey: 'UserId'});
 
-Holding.hasMany(Operation, { foreinkey: "HoldingId" });
-Operation.belongsTo(Holding, { foreignKey: 'HoldingId' });
+// Holding.hasMany(Operation, { foreinkey: "HoldingId" });
+// Operation.belongsTo(Holding, { foreignKey: 'HoldingId' });
 
-
-sequelize.sync({ force: false })
-    .then( () => {
-        app.listen(3001, async () => {
-            console.log('listening at 3001');
-            const difference = await getdifDate();
-            if( difference>=1 ) 
-                initialCriptoLoadingCMC();
+// sequelize.sync({ force: false })
+//     .then( () => {
+//         app.listen(3001, async () => {
+//             console.log('listening at 3001');
+//             const difference = await getdifDate();
+//             if( difference>=1 ) 
+//                 initialCriptoLoadingCMC();
             
-        });
-    })
+//         });
+//     })
+//     .catch( e => console.error(e) );
+
 // CREATE TABLE "Criptos2" AS SELECT * FROM "Criptos";
+
+const port = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+  
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
