4f00d6cc5de429ffb8b79a32b1b2e47b32172cf6
pablosingh <pablo22_4@hotmail.com>
2024-08-15 21:53:37 -0300

    ssl

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,13 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(DATABASE_URL,{
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+        ssl: {
+        require: true, // Requiere SSL
+        rejectUnauthorized: false // Establecer en true si el certificado es válido
+        }
+    }
 });
 
 export default sequelize;
